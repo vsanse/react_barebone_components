d5bd590627a46ce518eb48e951f5568bc4ffc1a4
Vishal Sanserwal <vsanse@outlook.com>
2021-09-21 21:04:29 +0530

    layout updates

--- src/App.js
+++ src/App.js
@@ -20,52 +20,71 @@ export default function App() {
   const [showModalLoading, setShowModalLoading] = React.useState(false);
   return (
     <div className="App">
-      <div className="App__grid2">
-        <Button>Default Button</Button>
-        <Button danger>Default Danger Button</Button>
-        <Button type="link">Link Button</Button>
-        <Button type="link" danger>
-          Danger Link Button
-        </Button>
-        <Button type="primary">Primary Button</Button>
-        <Button type="primary" danger>
-          Danger Primary Button
-        </Button>
-      </div>
-      <div className="App__grid2">
-        <Button type="primary" onClick={() => setShowModal(true)} shape="round">
-          Open Modal
-        </Button>
-        <Button
-          type="primary"
-          onClick={() => setShowModalLoading(true)}
-          shape="round"
-        >
-          Open Modal With Loading
-        </Button>
-        <Modal
-          visible={showModal}
-          onClose={() => setShowModal(false)}
-          width={300}
-        >
-          Hello World!
-        </Modal>
-        <Modal
-          visible={showModalLoading}
-          onClose={() => setShowModalLoading(false)}
-          width={300}
-        >
-          <LoadingCircle />
-        </Modal>
-      </div>
-      <div className="App__grid2">
-        <div>
-          <p className="App__demoTitle">Dropdown</p>
-          <Dropdown options={DATA} defaultOption={0} />
+      <div className="App__Section">
+        <p className="App__Section--title">Buttons</p>
+        <div className="App__grid2">
+          <Button>Default Button</Button>
+          <Button danger>Default Danger Button</Button>
+          <Button type="link">Link Button</Button>
+          <Button type="link" danger>
+            Danger Link Button
+          </Button>
+          <Button type="primary">Primary Button</Button>
+          <Button type="primary" danger>
+            Danger Primary Button
+          </Button>
+          <Button type="hollow" shape="round">
+            Primary Button
+          </Button>
+          <Button type="hollow" danger shape="round">
+            Danger Primary Button
+          </Button>
         </div>
-        <div>
-          <p className="App__demoTitle">Dropdown Autosuggest</p>
-          <AutoSuggest options={DATA} defaultOption={0} autoFocus={true} />
+      </div>
+      <div className="App__Section">
+        <p className="App__Section--title">Modals</p>
+        <div className="App__grid2">
+          <Button
+            type="primary"
+            onClick={() => setShowModal(true)}
+            shape="round"
+          >
+            Open Modal
+          </Button>
+          <Button
+            type="primary"
+            onClick={() => setShowModalLoading(true)}
+            shape="round"
+          >
+            Open Modal With Loading
+          </Button>
+          <Modal
+            visible={showModal}
+            onClose={() => setShowModal(false)}
+            width={300}
+          >
+            Hello World!
+          </Modal>
+          <Modal
+            visible={showModalLoading}
+            onClose={() => setShowModalLoading(false)}
+            width={300}
+          >
+            <LoadingCircle />
+          </Modal>
+        </div>
+      </div>
+      <div className="App__Section">
+        <p className="App__Section--title">Select</p>
+        <div className="App__grid2">
+          <div>
+            <p className="App__demoTitle">Dropdown</p>
+            <Dropdown options={DATA} defaultOption={0} />
+          </div>
+          <div>
+            <p className="App__demoTitle">Dropdown Autosuggest</p>
+            <AutoSuggest options={DATA} defaultOption={0} autoFocus={true} />
+          </div>
         </div>
       </div>
     </div>
