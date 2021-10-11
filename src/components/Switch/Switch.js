d61abe21844fe5820f8abf250c87abb9acf9eaf2
Vishal Sanserwal <vsanse@outlook.com>
2021-10-10 19:21:32 -0800

    initial commit

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -2,19 +2,24 @@ import React from "react";
 
 import "./Switch.scss";
 
-const Switch = ({ onChange, defaultValue, label, id }) => {
+const Switch = ({ onChange, defaultValue, label, id, icon }) => {
+  const ref = React.useRef(null);
   const handleChange = (evt) => {
     onChange && onChange(evt.target.checked);
   };
   return (
     <div className="switchBtn">
       <input
+        ref={ref}
         id={id}
         type="checkbox"
         className={`app__switch`}
         onChange={handleChange}
         defaultChecked={defaultValue}
       ></input>
+      <span className="switch__thumb" onClick={() => ref.current.click()}>
+        {icon && <span className="switch__thumb--icon">{icon}</span>}
+      </span>
       {label && <label htmlFor={id}>{label}</label>}
     </div>
   );
