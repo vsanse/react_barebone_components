741438295c6f93252d2f7145bdb3aaf6004d7f7a
Akshit <akshitdandyan@outlook.com>
2021-10-17 16:16:44 +0530

    paper-component optimize box shadow logic

--- src/components/Paper/Paper.jsx
+++ src/components/Paper/Paper.jsx
@@ -1,14 +1,19 @@
 import "./Paper.scss";
 
+function getBoxShadow( elevation, mode ){
+    return {
+        boxShadow:
+        `${ elevation ? elevation*2 : "6"}px ${ elevation ? elevation*2 : "6"}px ${ elevation ? elevation*4 : "6"}px 1px rgba(0,0,0,0.${mode === "dark" ? 3 : 1}),
+        -${ elevation ? elevation : "1"}px ${ elevation ? elevation : "1"}px ${ elevation ? elevation*2 : "6"}px 1px rgba(0,0,0,0.${mode === "dark" ? 3 : 1})
+        `
+    }
+}
+
 const Paper = ({ mode, elevation, children }) => {
     return (
         <div 
             className={`paper__wrapper ${mode === "dark" && "paper__dark"}`}
-            style={{
-                boxShadow: `${ elevation ? elevation*2 : "6"}px ${ elevation ? elevation*2 : "6"}px ${ elevation ? elevation*4 : "6"}px 1px rgba(0,0,0,0.${mode === "dark" ? 3 : 1}),
-                            -${ elevation ? elevation : "1"}px ${ elevation ? elevation : "1"}px ${ elevation ? elevation*2 : "6"}px 1px rgba(0,0,0,0.${mode === "dark" ? 3 : 1})
-                            `
-            }}
+            style={ getBoxShadow( elevation, mode ) }
         >
             { children }
         </div>
